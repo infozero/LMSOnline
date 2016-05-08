baf0866c0a235b87224930905c0ac34770e855ac
infozero <120458879@qq.com>
2016-05-08 19:03:42 +0800

    remove html

--- src/index.php
+++ src/index.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="GBK">
-        <title>这是示例程序</title>
-    </head>
-    <body>
-        显示服务器的版本信息。
         <?php
         // put your code here
         phpinfo();
         ?>
-    </body>
-</html>
